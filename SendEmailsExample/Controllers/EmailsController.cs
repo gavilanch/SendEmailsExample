1b829d35c64549dc985cd306618693e41502493c
Felipe Gavilan <elmejor_887@hotmail.com>
2024-10-23 19:58:48 -0400

    Add project files.

---
+++ SendEmailsExample/Controllers/EmailsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SendEmailsExample.Services;
+
+namespace SendEmailsExample.Controllers
+{
+    [Route("api/emails")]
+    [ApiController]
+    public class EmailsController: ControllerBase
+    {
+        private readonly IEmailService emailService;
+
+        public EmailsController(IEmailService emailService)
+        {
+            this.emailService = emailService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SendEmail(string receptor, string subject, string body)
+        {
+            await emailService.SendEmail(receptor, subject, body);
+            return Ok();
+        }
+    }
+}
